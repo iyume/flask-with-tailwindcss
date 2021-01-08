b0627b0a3e86b208f8fb356c0f9ced47f404a469
iyume <iyumelive@gmail.com>
2021-01-08 22:04:47 +0800

    Dated 2021-01-08

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 // do the customization
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: {
     enabled: false, // set to false if you are in developing
@@ -9,7 +11,24 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      white: '#ffffff',
+      gray: colors.trueGray,
+      yellow: colors.amber,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    extend: {
+      width: {
+        'normal': '1160px',
+      },
+      colors: {
+        'darkless': '#f8f8f8',
+        'darker': '#e9ebec',
+        'darkest': '#000000',
+        },
+    },
   },
   variants: {
     extend: {},
